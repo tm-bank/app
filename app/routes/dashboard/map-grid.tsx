e3c9ddf3248c2350473083b552205e98fa274e47
languint <longuint@proton.me>
2025-05-19 08:40:49 -0500

    fix: map grid

--- app/routes/dashboard/map-grid.tsx
+++ app/routes/dashboard/map-grid.tsx
@@ -1,17 +1,15 @@
-import { Eye } from "lucide-react";
-import { Badge } from "~/components/ui/badge";
-import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardFooter } from "~/components/ui/card";
 import { useAppSelector } from "~/store/store";
-import type { Map } from "~/types";
-import { deleteMap } from "~/store/db";
 import { useAuth } from "~/providers/auth-provider";
-import { Link } from "react-router";
 import { SceneryCard } from "~/components/scenery-grid";
+import { toast } from "sonner";
 
 export function MapGrid() {
   const maps = useAppSelector((state) => state.maps);
   const { user } = useAuth();
+
+  if (!maps) {
+    toast.error("Failed to get maps!");
+  }
 
   return (
     <div className="p-6">
@@ -27,7 +25,7 @@ export function MapGrid() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {maps.maps
-          .filter((map) => map.authorId == user?.id)
+          .filter((map) => map.authorId === user?.id)
           .map((item) => (
             <SceneryCard key={item.id} item={item} dashboard />
           ))}
